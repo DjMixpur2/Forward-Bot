846874c7f3c531055d822224ccade416afd10d0a
DjMixpur2 <djmixpur2@gmail.com>
2024-11-12 20:58:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/DjMixpur2/Ultra-Forward-Bot DjMixpur2/Ultra-Forward-Bot
+  git clone https://github.com/DjMixpur2/Forward-Bot DjMixpur2/Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/DjMixpur2/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/DjMixpur2/Forward-Bot -b $BRANCH /Ultra-Forward-Bot
 fi
-cd DjMixpur2/Ultra-Forward-Bot
+cd DjMixpur2/Forward-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
