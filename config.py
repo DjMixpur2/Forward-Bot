a4cb8daee03860288c08d9ec2370711251adf498
DjMixpur2 <djmixpur2@gmail.com>
2024-11-14 18:18:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "26838876")
-    API_HASH = os.environ.get("API_HASH", "6cdbafc28d308bda79818bcdb0d2cd99")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8178732396:AAHRh6IDq8eIAFB2W-uRLJO-bh6bIXNAbwc") 
+    API_ID = os.environ.get("API_ID", "2")
+    API_HASH = os.environ.get("API_HASH", "6cdbafc28d308bda79818bcdb09")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8178732396:AAHRh6IDquRLJO-bh6bIXNAbwc") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://djmixpur2:djmixpur2@cluster0.7i3sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://djmixter0.7i3sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7669685452').split()]
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '766952').split()]
 
 
 class temp(object): 
