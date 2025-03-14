60c8c53ed2678ac8fb33f4cb9caa7fdc15c18677
DjMixpur2 <djmixpur2@gmail.com>
2025-03-14 22:05:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import datetime
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = environ.get("API_ID", "21344245")
+    API_HASH = environ.get("API_HASH", "e72de40c666f1664f847a79f97dd3882")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7980904477:AAHQOHBwPES7dkDrgHPF40_leghWYQbIoFo") 
     BOT_SESSION = environ.get("BOT_SESSION", "Forward-Bot") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://djmixpuryt:djmixpuryt@cluster0.2qrq1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7585839477').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
    
 class temp(object): 
